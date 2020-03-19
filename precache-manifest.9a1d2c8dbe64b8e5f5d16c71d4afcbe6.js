97028c297775eb1ea40e6b161969cecd425010ff
tz01x <abdur963rahman@gmil.com>
2020-03-19 10:57:37 +0600

    Updates

---
+++ precache-manifest.9a1d2c8dbe64b8e5f5d16c71d4afcbe6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1cfaa3cdc236c760f2c2a85c05995c1",
+    "url": "/test_my-app/index.html"
+  },
+  {
+    "revision": "47c129a6f69baba8fd7e",
+    "url": "/test_my-app/static/css/main.44144cfb.chunk.css"
+  },
+  {
+    "revision": "c56b56d5281633386695",
+    "url": "/test_my-app/static/js/2.e69d0181.chunk.js"
+  },
+  {
+    "revision": "47c129a6f69baba8fd7e",
+    "url": "/test_my-app/static/js/main.792402ac.chunk.js"
+  },
+  {
+    "revision": "dbce0bc3caf09c07b37d",
+    "url": "/test_my-app/static/js/runtime~main.13813b5b.js"
+  }
+]);
